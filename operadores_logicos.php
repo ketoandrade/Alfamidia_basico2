a2e67e1d9b5bd3d0a9526ebf2b734363c9b853be
noite <noite@DPC2013-5.aluno.local>
2016-08-23 22:28:46 -0300

    ATUALIZAÇÃO DA AULA

---
+++ operadores_logicos.php
@@ -0,0 +1,28 @@
+<?php
+
+$a=true;
+$b=true;
+
+if($a and $b){
+        
+      echo "se A e B forem verdadeiros escreva TRUE<br/>";  
+    }
+if($a or $b){
+        
+      echo "se A ou B forem verdadeiros escreva TRUE<br/>";  
+    }
+
+if($a xor $b){
+        
+      echo "se A ou B forem verdadeiros exclua 1 false<br/>";  
+    }
+    
+if($a && $b){
+        
+      echo "se A e B forem verdadeiros escreva TRUE<br/>";  
+    }
+    
+if($a || $b){
+        
+      echo "se A ou B forem verdadeiros escreva TRUE<br/>";  
+    }
